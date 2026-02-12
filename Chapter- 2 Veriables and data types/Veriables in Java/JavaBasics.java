2cdad631a9f3694cf0b0dc6697aea413686be414
vansh <vanshdhaka02@gmail.com>
2026-02-13 02:40:12 +0530

    Veriables in Java

---
+++ Chapter- 2 Veriables and data types/Veriables in Java/JavaBasics.java
@@ -0,0 +1,15 @@
+public class JavaBasics{
+  public static void main(String args[]){
+    int a = 10;                          // a is veriable
+    int b = 14;
+    System.out.println(a);
+    System.out.println(b);
+
+    String name = "vansh";               // name is veriable
+    System.out.println(name);
+
+    a = b;                               // we can iterate the value of the veriable
+    System.out.println(a);
+  } 
+}
+//Veriables in Java
