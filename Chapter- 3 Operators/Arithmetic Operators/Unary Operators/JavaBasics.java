d00c9ec417d4ade2f873ce7de34c7220fda92ae9
vansh <vanshdhaka02@gmail.com>
2026-02-14 00:58:12 +0530

    Arithmetic Operators

---
+++ Chapter- 3 Operators/Arithmetic Operators/Unary Operators/JavaBasics.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+public class JavaBasics{
+  public static void main(String args[]){
+    //pre increment
+    int a = 10;
+    int b = ++a;            
+    System.out.println(a);
+    System.out.println(b);
+
+    //post increment
+    int a = 10;
+    int b = a++;            
+    System.out.println(a);
+    System.out.println(b);
+
+
+    //pre decrement
+    int a = 10;
+    int b = --a;            
+    System.out.println(a);
+    System.out.println(b);
+
+    //post decrement
+    int a = 10;
+    int b = a--;            
+    System.out.println(a);
+    System.out.println(b);
+  } 
+}
+//Arithmetic Operators (Unary)
