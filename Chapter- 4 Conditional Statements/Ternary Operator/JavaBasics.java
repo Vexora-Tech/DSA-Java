544ff98427aed4036970065fdd32bba0b69f3c46
vansh <vanshdhaka02@gmail.com>
2026-02-15 14:25:50 +0530

    Ternary Operator

---
+++ Chapter- 4 Conditional Statements/Ternary Operator/JavaBasics.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class JavaBasics{
+  public static void main(String args[]){
+    int number = 10;
+
+    String type = ((number % 2) == 0) ? "Even" : "Odd";
+    System.out.println(type);
+  } 
+}
+//Ternary Operator 
