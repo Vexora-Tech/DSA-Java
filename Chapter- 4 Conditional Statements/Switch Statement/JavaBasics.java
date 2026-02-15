4ed9ce8f58d4cb6bf9c7feadcdbf18a5417fe876
vansh <vanshdhaka02@gmail.com>
2026-02-15 14:25:01 +0530

    Switch Statement

---
+++ Chapter- 4 Conditional Statements/Switch Statement/JavaBasics.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class JavaBasics{
+  public static void main(String args[]){
+    int number = 1;
+    switch(number){
+      case 1 :System.out.println("samosa");
+            break;
+      case 2 :System.out.println("pizza");
+            break;
+      case 3 :System.out.println("burgar");
+            break;
+      default : System.out.println("wake up");      
+    }
+  } 
+}
+//Switch Statement
