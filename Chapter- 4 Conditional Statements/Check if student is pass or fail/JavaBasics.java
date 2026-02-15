a2ced591ee32851ec6f657359073871cf8db6466
vansh <vanshdhaka02@gmail.com>
2026-02-15 14:17:00 +0530

    Check if student is pass or fail

---
+++ Chapter- 4 Conditional Statements/Check if student is pass or fail/JavaBasics.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class JavaBasics{
+  public static void main(String args[]){
+    int marks = 89;
+
+    String reportCard = (marks >= 33) ? "PASS" : "Fail";
+    System.out.println(reportCard);
+  } 
+}
+//Check if student is Pass or fail
