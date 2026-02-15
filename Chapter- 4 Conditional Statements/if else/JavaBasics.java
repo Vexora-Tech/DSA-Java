f4db053c604dbcdd75bf16ae03a8dc611c0e894e
vansh <vanshdhaka02@gmail.com>
2026-02-15 14:18:30 +0530

    if else

---
+++ Chapter- 4 Conditional Statements/if else/JavaBasics.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class JavaBasics{
+  public static void main(String args[]){
+    int age = 20;
+    if(age >= 18){
+      System.out.println("adult");
+    }
+
+    else{
+      System.out.println("not adult");
+    }
+  } 
+}
+//if else 
