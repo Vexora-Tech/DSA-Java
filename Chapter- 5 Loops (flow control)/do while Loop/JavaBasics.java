48e58570efa0e9e97eb59b015695cb00c2664964
vansh <vanshdhaka02@gmail.com>
2026-02-16 00:47:28 +0530

    do while Loop

---
+++ Chapter- 5 Loops (flow control)/do while Loop/JavaBasics.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class JavaBasics{
+  public static void main(String args[]){
+    int counter = 1;
+    do {
+      System.out.println("hello world!");
+      counter++;
+    }
+    while(counter <= 10);
+  } 
+}
+//do while Loop
+
+
