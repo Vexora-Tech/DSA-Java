7e45e0c6965f3d4241259695a8f5d605f85777e4
vansh <vanshdhaka02@gmail.com>
2026-02-15 14:23:42 +0530

    Print largest of three numbers

---
+++ Chapter- 4 Conditional Statements/Print largest of three numbers/JavaBasics.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class JavaBasics{
+  public static void main(String args[]){
+    int a = 1, b = 3, c = 6;
+
+    if((a >= b) && (a >= c)){
+      System.out.println("a is largest");
+    }else if((b >= c)){
+      System.out.println("b is largest");
+    }else{
+      System.out.println("c is largest");
+    } 
+  }
+} 
+//Print largest of three numbers 
