694bfe2d499ce1e4ff3f78d384f143dee230b8c0
vansh <vanshdhaka02@gmail.com>
2026-02-16 00:48:32 +0530

    for Loop

---
+++ Chapter- 5 Loops (flow control)/for Loop/JavaBasics.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class JavaBasics{
+  public static void main(String args[]){
+    for(int i=1; i<=10; i++){
+      System.out.println("hello world!");
+    }
+    System.out.println("printed 10x times!");
+  } 
+}
+//for Loop
+
+
