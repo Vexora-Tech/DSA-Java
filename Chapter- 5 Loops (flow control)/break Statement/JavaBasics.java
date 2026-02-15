c6a344050a186946ac9721fbffd6cbb4ae8cb7ff
vansh <vanshdhaka02@gmail.com>
2026-02-16 00:45:10 +0530

    break Statement

---
+++ Chapter- 5 Loops (flow control)/break Statement/JavaBasics.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class JavaBasics{
+  public static void main(String args[]){
+    for(int i=1; i<=5; i++){
+      if(i == 3){
+        break;
+      }
+    System.out.println(i);
+    }
+    System.out.print("I'm out of the Loop");
+  } 
+}
+//break Statement
+
+
