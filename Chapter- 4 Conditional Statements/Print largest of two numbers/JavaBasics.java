63319619ed036e3dbde20f9becad8628047d7b7a
vansh <vanshdhaka02@gmail.com>
2026-02-15 14:24:32 +0530

    Print largest of two numbers

---
+++ Chapter- 4 Conditional Statements/Print largest of two numbers/JavaBasics.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class JavaBasics{
+  public static void main(String args[]){
+    int a = 20;
+    int b = 40;
+    
+    if(a >= b){
+      System.out.println("a is largest");
+    }else{
+      System.out.println("b is largest");
+    }
+  } 
+}
+//Print largest of two numbers 
