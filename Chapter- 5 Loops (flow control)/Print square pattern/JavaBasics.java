b1378ecdeec89ee2e36fe8b6e100d8dc29010750
vansh <vanshdhaka02@gmail.com>
2026-02-17 01:04:56 +0530

    Print square pattern

---
+++ Chapter- 5 Loops (flow control)/Print square pattern/JavaBasics.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class JavaBasics{
+  public static void main(String args[]){
+    //from for Loop
+    for(int i=1; i<=4; i++){
+      System.out.println("* * * *");
+    }
+
+    //from while Loop
+    int i = 1;
+    while(i<=4){
+      System.out.println("* * * *");
+      i++;
+    }
+  } 
+}
+//Print Square print
+
+
