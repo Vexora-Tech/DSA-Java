14e344e67ab3719a2973ac4a79b089b2a6ede390
vansh <vanshdhaka02@gmail.com>
2026-02-17 00:59:49 +0530

    Loops Basics

---
+++ Chapter- 5 Loops (flow control)/Loops Basics/JavaBasics.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class JavaBasics{
+  public static void main(String args[]){
+    System.out.print("hello world!");
+    System.out.print("hello world!");
+    System.out.print("hello world!");
+  } 
+}
+//Loops Basics:
+// while Loops
+// for Loops
+// do while Loops
+
