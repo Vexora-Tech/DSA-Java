657e083b0a504e07387e4fd04d2e76541c4c7800
vansh <vanshdhaka02@gmail.com>
2026-02-17 01:00:53 +0530

    Print numbers from 1 to 10

---
+++ Chapter- 5 Loops (flow control)/Print numbers from 1 to 10/JavaBasics.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class JavaBasics{
+  public static void main(String args[]){
+    int counter = 1;
+    while(counter <= 10){
+      System.out.println(counter);
+      counter++;
+    }
+    System.out.println("printed numbers from 1 to 10");
+  } 
+}
+//Print number from 1 to 10
+
+
