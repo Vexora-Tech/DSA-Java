0c6c708f653b81d7e43cd61d8ea744b8daee3d77
vansh <vanshdhaka02@gmail.com>
2026-02-18 00:53:31 +0530

    Chapter - 3 Operators

---
+++ Chapter - 3 Operators/Arithmetic Operators/Binary Operators/JavaBasics.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class JavaBasics{
+  public static void main(String args[]){
+    int a = 10;
+    int b = 5;
+    System.out.println("sum = " + (a+b));
+    System.out.println("subtract = " + (a-b));
+    System.out.println("multiply = " + (a*b));
+    System.out.println("divide = " + (a/b));
+    System.out.println("modulo(remainder) = " + (a%b));
+  } 
+}
+//Arithmetic Operators (Binary)
