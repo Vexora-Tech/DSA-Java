351e2e17ae0ee967ef0f2865a8ccc1d107d2e127
vansh <vanshdhaka02@gmail.com>
2026-02-18 02:41:28 +0530

    Intro to Nested Loop (star pattern)

---
+++ Chapter- 6 Patterns (part I)/Intro to Nested Loop (star pattern)/JavaBasics.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class JavaBasics{
+  public static void main(String args[]){
+    //from for Loop
+    for(int i=1; i<=4; i++){
+      for(int j=1; j<=i; j++){
+        System.out.print("*");
+      }
+      System.out.println();
+    }
+  } 
+}
+//Intro to nested Loop (star pattern)
+
+
