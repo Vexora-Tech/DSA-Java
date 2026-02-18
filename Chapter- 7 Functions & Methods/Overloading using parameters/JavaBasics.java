d0f8b13280bb3bd2239235769e55730c91a8b397
vansh <vanshdhaka02@gmail.com>
2026-02-19 01:16:46 +0530

    Overloading using parameters

---
+++ Chapter- 7 Functions & Methods/Overloading using parameters/JavaBasics.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class JavaBasics{
+  //func to cal sum of 2 nums
+  public static int sum(int a, int b){
+    return a + b;
+  }
+
+  //func to cal sum of 3 nums
+  public static int sum(int a, int b, int c){
+    return a + b + c;
+  }
+  public static void main(String args[]){
+    System.out.println(sum(1, 3));
+    System.out.println(sum(1, 1, 2));
+  } 
+}
+//Overloading using parameters
+
