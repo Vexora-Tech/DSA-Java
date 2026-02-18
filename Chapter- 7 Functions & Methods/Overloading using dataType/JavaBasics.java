8680cc35b75e4f86864c6e469b39cf3ef3b39ebd
vansh <vanshdhaka02@gmail.com>
2026-02-19 01:14:17 +0530

    Overloading using dataType

---
+++ Chapter- 7 Functions & Methods/Overloading using dataType/JavaBasics.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class JavaBasics{
+  //func to cal sum of 2 nums
+  public static int sum(int a, int b){
+    return a + b;
+  }
+
+  //func to cal sum of 3 nums
+  public static float sum(float a, float b){
+    return a + b;
+  }
+  public static void main(String args[]){
+    System.out.println(sum(4, 4));
+    System.out.println(sum(2.6f, 5.4f));
+  } 
+}
+//Overloading using dataType
+
