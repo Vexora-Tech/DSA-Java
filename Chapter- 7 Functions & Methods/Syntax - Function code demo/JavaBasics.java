9c1ae4dacbffb5ff9bf1827a7d9eca84822c3b06
vansh <vanshdhaka02@gmail.com>
2026-02-19 01:19:36 +0530

    Syntax - Function code demo

---
+++ Chapter- 7 Functions & Methods/Syntax - Function code demo/JavaBasics.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class JavaBasics{
+  public static void printHelloWorld(){
+    System.out.println("Hello World!");
+    System.out.println("Hello World!");
+    System.out.println("Hello World!");
+  }
+  public static void main(String args[]){
+    printHelloWorld();    //function call
+  } 
+}
+//Syntax - Function code demo
+
+//syntax
+//returnType name(){
+    //body
+    //return statement;
+//}
+
