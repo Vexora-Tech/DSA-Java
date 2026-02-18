5b58fc50bdd948358297723a0a8ff1f1818b6f4b
vansh <vanshdhaka02@gmail.com>
2026-02-19 01:17:19 +0530

    Parameters & arguments

---
+++ Chapter- 7 Functions & Methods/Parameters & arguments/JavaBasics.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class JavaBasics{
+  public static int calculateSum(int num1, int num2){   //parameters or formal parameters
+    int sum = num1 + num2;
+    return sum;
+  }
+  public static void main(String args[]){
+    Scanner sc = new Scanner(System.in);
+    int a = sc.nextInt();
+    int b = sc.nextInt();
+    int sum = calculateSum(a, b);   //arguments or actual parameters
+    System.out.println(sum);
+  } 
+}
+//Syntax with Parameters
+
+//syntax
+//returnType name(type param1, type param2){
+    //body
+    //return statement;
+//}
+
