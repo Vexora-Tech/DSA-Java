2434e4abb70cf6cf4972d1c9dea8cdee4082fa8f
vansh <vanshdhaka02@gmail.com>
2026-02-22 04:23:55 +0530

    Arrays - creation

---
+++ Chapter- 9 ARRAYS (part I)/Arrays - creation/ArraysCC.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class ArraysCC{
+  public static void main(String args[]){
+    //creation of ARRAYS
+
+    int marks[] = new int[50];
+
+    int numbers = {1, 2, 3, 4};
+
+    int moreNumbers[] = {4, 5, 6, 8};
+
+    String fruits[] = {"apple", "banana", "mango", "orange"}; 
+  }
+}
+//Arrays - creation
+
