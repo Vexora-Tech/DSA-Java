8c53fdca0e1a4804ad0952fdca2b3bd415389162
vansh <vanshdhaka02@gmail.com>
2026-03-04 03:54:25 +0530

    String Concatenate

---
+++ Chapter-13 Strings/String Concatenate/Strings.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class Strings{
+  public static void main(String args[]){
+    //Concatenation
+    String firstName = "Tony";
+    String lastName = "Stark";
+    String fullName = firstName + " " + lastName;
+    System.out.println(fullName);
+  }
+}
+//String Concatenate
