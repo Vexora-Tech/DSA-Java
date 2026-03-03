7709872c8c0fd08555d841815fe48f79e717a67c
vansh <vanshdhaka02@gmail.com>
2026-03-03 23:10:52 +0530

    Input & Output

---
+++ Chapter-13 Strings/Input & Output/Strings.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Strings{
+  public static void main(String args[]){
+    Scanner sc = new Scanner(System.in);
+    String name;
+
+    // name = sc.next();   //takes only a word
+    name = sc.nextLine();   //takes whole line
+    System.out.println(name);
+  }
+}
+//Input & Output
