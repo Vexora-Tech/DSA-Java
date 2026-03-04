695e2b8c507cfac52448b0664107099f8e752f8c
vansh <vanshdhaka02@gmail.com>
2026-03-05 00:17:35 +0530

    String Length

---
+++ Chapter-13 Strings/String Length/Strings.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class Strings{
+  public static void main(String args[]){
+    //Length of a String
+    String fullName = "Tony Stark";
+    System.out.println(fullName.length());
+  }
+}
+//String Length
