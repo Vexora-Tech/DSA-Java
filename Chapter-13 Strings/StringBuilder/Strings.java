5d6459813793c8de6a6c5263afb1bf23606a72e1
vansh <vanshdhaka02@gmail.com>
2026-03-05 00:17:55 +0530

    StringBuilder

---
+++ Chapter-13 Strings/StringBuilder/Strings.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Strings{
+  public static void main(String args[]){
+    StringBuilder sb = new StringBuilder("");
+
+    for(char ch='a'; ch<'z'; ch++){
+      sb.append(ch);
+    }
+    System.out.println(sb);
+  }
+}
+//StringBuilder
