845aaf587816c89def4fe672d6ced2b94c455bec
vansh <vanshdhaka02@gmail.com>
2026-03-05 00:18:13 +0530

    What are Strings?

---
+++ Chapter-13 Strings/What are Strings?/Strings.java
@@ -0,0 +1,11 @@
+public class Strings{
+  public static void main(String args[]){
+    //declaration of Strings
+    char arr[] = {'a','b','c','d'};
+    String str = "abcd";
+    String str2 = new String("xyz");
+
+    //Strings are IMMUTABLE
+  }
+}
+//What Are Strings?
