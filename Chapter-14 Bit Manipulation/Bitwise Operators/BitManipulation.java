0784271e7cbe20d917bc45db2d8214f8cd61ad25
vansh <vanshdhaka02@gmail.com>
2026-03-06 01:37:16 +0530

    Bitwise Operators

---
+++ Chapter-14 Bit Manipulation/Bitwise Operators/BitManipulation.java
@@ -0,0 +1,23 @@
+public class BitManipulation{
+  public static void main(String args[]){
+    //Binary AND
+    System.out.println((5 & 6));
+
+    //Binary OR 
+    System.out.println((5 | 6));
+
+    //Binary XOR 
+    System.out.println((5 ^ 6));
+
+    //Binary 1's Complement 
+    System.out.println((~5));
+    System.out.println((~0));  // -1
+  
+    //Binary Left Shift
+    System.out.println((5<<2));
+
+    //Binary right Shift
+    System.out.println((6>>1));
+  }
+}
+//Bitwise Operators
