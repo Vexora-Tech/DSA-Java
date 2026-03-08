eebf8282ba5e1f86391fda4fb67f9671ac4f644e
Vansh dhaka <vanshdhaka02@gmail.com>
2026-03-08 19:49:36 +0530

    Print x to the power n

---
+++ Chapter-16 Recursion Basics (Part I)/Print x to the power n/RecursionBasics.java
@@ -0,0 +1,17 @@
+public class RecursionBasics{
+  public static int power(int x, int n) {
+    if (n == 0){
+      return 1;
+    }
+
+    // int xnm1 = power(x, n-1);
+    // int xn = x * xnm1;
+    // return xn;
+
+    return x * power(x, n-1);
+  }  
+  public static void main(String args[]){
+    System.out.print(power(2, 10));
+  }
+}
+//Print x to the power n
