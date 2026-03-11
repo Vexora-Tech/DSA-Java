8625453e523f7dd5bbb21184a30c830db355ac7d
Vansh dhaka <vanshdhaka02@gmail.com>
2026-03-12 01:48:43 +0530

    What is Divide & Conquer?

---
+++ Chapter-18 Divide & Conquer/What is Divide & Conquer?/DividenConquer.java
@@ -0,0 +1,11 @@
+public class DivideConquer{
+  public static void printArr(String args[]){
+  // Divide and conquer is an algorithmic paradigm where a problem is broken into smaller subproblems, each subproblem is solved independently, and the results are combined to solve the original problem.
+
+  // The Three Steps
+  //1. Divide – Split the problem into smaller subproblems.
+  //2. Conquer – Solve each subproblem (often recursively).
+  //3. Combine – Merge the results into a final solution.
+  }
+}
+//What is Divide & Conquer?
