31ce41038604f2f4b1c7cfc8016e24c23d4c515a
Vansh dhaka <vanshdhaka02@gmail.com>
2026-08-24 19:08:18 +0530

    Head and Tail in LL

---
+++ Chapter-22 Linked List (Part I)/Head and Tail in LL/LinkedList.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class LinkedList{
+  public static class Node {
+    int data;
+    Node next;
+
+    public Node(int data){
+      this.data = data;
+      this.next = null;
+    }
+  }
+
+  public static Node head;
+  public static Node tail;
+
+  public static void main(String args[]){
+    LinkedList ll = new LinkedList();
+    //write methods to head and remove ll
+  }
+}
+//Head and Tail in ll
