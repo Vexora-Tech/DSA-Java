5c9eb67a322e5c130ab59313473a48844a29ba82
Vansh dhaka <vanshdhaka02@gmail.com>
2026-08-24 19:17:32 +0530

    Size of a LL

---
+++ Chapter-22 Linked List (Part I)/Size of a LL/LinkedList.java
@@ -0,0 +1,54 @@
+import java.util.*;
+public class LinkedList{
+  public static class Node {
+    int data;
+    Node next;
+
+    public Node(int data){
+      this.data = data;
+      this.next = null;
+    }
+  }
+
+  public static Node head;
+  public static Node tail;
+
+  public static int size;
+
+  public void addFirst(int data){
+    //step 1 - create new Node
+    Node newNode = new Node(data);
+    size++;
+
+    if(head == null){
+      head = tail = newNode;
+      return;
+    }
+
+    //step 2 - newNode next = head
+    newNode.next = head;
+
+    //step 3 - head = newNode
+    head = newNode;  
+  }
+
+  public void print(){   //O(n)
+    Node temp = head;
+    while(temp != null){
+      System.out.print(temp.data + "->");
+      temp = temp.next;
+    }
+    System.out.println("null");
+  }
+
+  public static void main(String args[]){
+    LinkedList ll = new LinkedList();
+    ll.addFirst(4);
+    ll.addFirst(3);
+    ll.addFirst(2);
+    ll.addFirst(1);
+    ll.print();
+    System.out.println("size of a LL " + ll.size);
+  }
+}
+//Size of a LL
