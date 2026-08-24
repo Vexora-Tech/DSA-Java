d4eaa1b788bd7d0ef8027ecd7ebc555eeef9df6c
Vansh dhaka <vanshdhaka02@gmail.com>
2026-08-24 19:08:41 +0530

    Introduction to Linked list

---
+++ Chapter-22 Linked List (Part I)/Introduction to Linked list/LinkedList.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class LinkedList{
+  public static void main(String args[]){
+
+  // Linked ListsLinked Lists
+
+  // A linked list is a linear data structure where elements (called nodes) are stored in sequence, but unlike arrays, they aren't stored in contiguous memory locations. Instead, each node holds data and a pointer to the next node.
+  
+  // Structure of a Node
+  // [ Data | Next ] --> [ Data | Next ] --> [ Data | Next ] --> NULL
+  
+  // Each node contains:
+  // Data — the value stored
+  // Next — a pointer/reference to the next node
+  }
+}
+
+//Introduction to Linked list
