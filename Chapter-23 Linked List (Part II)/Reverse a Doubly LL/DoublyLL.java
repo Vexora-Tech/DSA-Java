406a412f10be4992a49e4fa8071045167d639ee6
Vansh dhaka <vanshdhaka02@gmail.com>
2026-08-26 14:34:43 +0530

    Reverse a Doubly LL

---
+++ Chapter-23 Linked List (Part II)/Reverse a Doubly LL/DoublyLL.java
@@ -0,0 +1,74 @@
+import java.util.*;
+public class DoublyLL{
+    public static class Node {
+    int data;
+    Node next;
+    Node prev;
+
+    public Node(int data){
+      this.data = data;
+      this.next = null;
+      this.prev = null;
+    }
+  }
+
+  public static Node head;
+  public static Node tail;
+  public static int size;
+
+  //Add
+  public void addFirst(int data){
+  Node newNode = new Node(data);
+  size++;
+  if(head == null){
+    head = tail = newNode;
+    return;
+  }
+
+  newNode.next = head;
+  head.prev = newNode; 
+  head = newNode;  
+  }
+
+  //Print
+  public void print(){   //O(n)
+    Node temp = head;
+    while(temp != null){
+      System.out.print(temp.data + "<->");
+      temp = temp.next;
+    }
+    System.out.println("null");
+  }
+
+  public void reverse(){
+    Node curr = head;
+    Node prev = null;
+    Node next;
+
+    while(curr != null){
+      next = curr.next;
+      curr.next = prev;
+      curr.prev = next;
+
+      prev = curr;
+      curr = next;
+    }
+    head = prev;
+  }
+
+  public static void main(String args[]){
+    DoublyLL dll = new DoublyLL();
+    dll.addFirst(3);
+    dll.addFirst(2);
+    dll.addFirst(1);
+
+    dll.print();
+    System.out.println(dll.size);
+    
+    dll.reverse();
+    dll.print();
+
+  }
+}
+
+//Reverse a Doubly LL
